5b4bd8c16571cadd72e7c2b0c5bfc874dbffbb1e
Ernesto Cobos <ernesto@cobos.io>
2020-07-22 13:14:01 -0500

    I removed unnecessary classes
    Signed-off-by: Ernesto Cobos <ernesto@cobos.io>

--- src/components/layout-fullscreen.js
+++ src/components/layout-fullscreen.js
@@ -7,7 +7,7 @@ const LayoutFullScreen = ({ children }) => {
   return (
     <>
       <div className="full-height is-relative has-background-blue">
-        <main className="is-vertical-center">{children}</main>
+        <main>{children}</main>
       </div>
     </>
   )
