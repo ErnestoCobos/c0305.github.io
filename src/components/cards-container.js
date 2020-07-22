c8f138b982ddbb48f3cb5e98c7ade2d9e9663af0
Ernesto Cobos <ernesto@cobos.io>
2020-07-22 11:49:16 -0500

    WIP of the card.js and cards-container.js component.
    Signed-off-by: Ernesto Cobos <ernesto@cobos.io>

---
+++ src/components/cards-container.js
@@ -0,0 +1,29 @@
+import React from "react"
+import "../sass/cards-container.scss"
+
+const CardContainer = () => {
+  return (
+    <>
+      <div className="cards__container">
+        <div className="cards cards-1 span-4">
+          <div className="cards__img">
+            <img alt="" src="https://dummyimage.com/600x600/000/fff"/>
+          </div>
+          <div className="cards__content">
+            <div className="cards__title">
+
+            </div>
+            <div className="cards__text">
+
+            </div>
+            <div className="cards__footer">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CardContainer
