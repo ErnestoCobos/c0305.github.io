8e402ef95bb78e1aaeda002bb455d8c607d9fa04
Ernesto Cobos <ernesto@cobos.io>
2020-07-22 11:10:43 -0500

    Added the titleAndSub Component
    Signed-off-by: Ernesto Cobos <ernesto@cobos.io>

---
+++ src/components/titleAndSub.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "../sass/_titleAndSub.scss"
+import "../sass/global.scss"
+
+const TitleAndSub = ({ title, sub, secsub }) => (
+  <div className="full-width text-center">
+    <h1 className="title">{title}</h1>
+    <div className="text-container">
+      <h4 className="sub-title">{sub}</h4>
+    </div>
+    <h6>{secsub}</h6>
+  </div>
+)
+
+export default TitleAndSub
